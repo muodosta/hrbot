d32bf6b27523ddf4291a7eec2a16d5b422d79cc0
DisOut <vatkin332@gmail.com>
2023-06-19 17:20:38 +0700

    0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="hrbot",
-    version="0.1.0",
+    version="0.1.1",
     author="MuoDosta",
     author_email="MuoDostaWork@gmail.com",
     description="The hrbot is a wrapper on top of the "
